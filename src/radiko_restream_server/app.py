e34c2952466f3d946d567a1b8057e56b2a8e36d4
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2026-08-13 13:45:44 +0000

    Added the logging.

--- src/radiko_restream_server/app.py
+++ src/radiko_restream_server/app.py
@@ -4,8 +4,10 @@ import argparse
 import os
 import shutil
 import subprocess
+import sys
 import threading
 import time
+from logging import INFO, Formatter, StreamHandler, getLogger
 
 import ffmpeg
 from flask import (
@@ -15,24 +17,34 @@ from flask import (
     send_from_directory,
 )
 from streamlink.session.session import Streamlink
+from streamlink.stream.stream import StreamIO
 from werkzeug.exceptions import BadRequest, NotFound
+
+APP_VERSION = "0.0.1"
+
+logger = getLogger(__name__)
+handler = StreamHandler(sys.stdout)
+formatter = Formatter(
+    fmt="[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %z",
+)
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(INFO)
 
 app = Flask(__name__)
 
 HOST = "0.0.0.0"
 PORT = 8888
 
-BUFF_SIZE = 1 << 16
-
 HLS_DIR = os.path.abspath(os.path.join("hls", "radiko"))
 PLAYLIST_NAME = "live.m3u8"
 PLAYLIST_PATH = os.path.join(HLS_DIR, PLAYLIST_NAME)
-
 TIMEOUT_SECONDS = 15
-
 FFMPEG_ARGS = {"c": "copy"}
 FFMPEG_ARGS_FOR_RADIKO = {"c:a": "aac", "b:a": "128k", "ar": "44100", "ac": "1"}
-
+BUFF_SIZE = 1 << 16
 
 EXCEPTION_MESSAGE = "Exception {0}: {1}\n"
 
@@ -52,13 +64,15 @@ def start(station_id: str) -> bool:
         monitor_thread_running, \
         bridge_thread_running
 
+    logger.info(f"Starting HTTP Live Streaming for {station_id}")
+
     stop()
 
     try:
         streams = Streamlink().streams(f"https://radiko.jp/#!/live/{station_id}")
 
         if not streams or "best" not in streams:
-            print(f"{station_id} is not found.")
+            logger.warning(f"{station_id} is not found")
             return False
 
         stream = streams["best"]
@@ -106,15 +120,17 @@ def start(station_id: str) -> bool:
             t = threading.Thread(target=monitor, daemon=True)
             t.start()
 
+        logger.info(f"Started HTTP Live Streaming for {station_id}")
+
         return True
 
     except OSError as e:
-        print(f"Error file/stream operation: {e}")
+        logger.error(f"File/Stream operation error: {e}")
         stop()
         return False
 
     except subprocess.SubprocessError as e:
-        print(f"Error process execute failed: {e}")
+        logger.error(f"Process error: {e}")
         stop()
         return False
 
@@ -122,28 +138,32 @@ def start(station_id: str) -> bool:
 def stop() -> None:
     global active_ffmpeg_process, active_stream_fd, bridge_thread_running
 
+    logger.info("Stopping HTTP Live Streaming")
+
     with lock:
         bridge_thread_running = False
 
         if active_ffmpeg_process:
-            print("Stoping streaming.")
-
             if active_ffmpeg_process.stdin is not None:
                 try:
                     active_ffmpeg_process.stdin.close()
 
-                except BrokenPipeError, OSError, ValueError:
-                    print("Error failed to close ffmpeg stdin.")
+                except (BrokenPipeError, OSError, ValueError) as e:
+                    logger.error(f"FFmpeg stdin close error: {e}")
 
             try:
                 active_ffmpeg_process.terminate()
                 active_ffmpeg_process.wait(timeout=2)
 
-            except subprocess.TimeoutExpired:
+            except subprocess.TimeoutExpired as e:
+                logger.warning(
+                    f"FFmpeg process was not terminated within the time limit: {e}"
+                )
+
                 active_ffmpeg_process.kill()
 
             except OSError as e:
-                print(f"Error terminating process: {e}")
+                logger.error(f"FFmpeg process terminate error: {e}")
 
             active_ffmpeg_process = None
 
@@ -152,7 +172,7 @@ def stop() -> None:
                 active_stream_fd.close()
 
             except OSError as e:
-                print(f"Error closing stream: {e}")
+                logger.error(f"Streamlink stream close error: {e}")
 
             active_stream_fd = None
 
@@ -161,11 +181,13 @@ def stop() -> None:
 
         os.makedirs(HLS_DIR)
 
+    logger.info("Stoped HTTP Live Streaming")
+
 
 def monitor() -> None:
     global monitor_thread_running
 
-    print("Start the timeout monitoring thread.")
+    logger.info("Starting timeout monitor thread")
 
     while True:
         time.sleep(2)
@@ -176,7 +198,9 @@ def monitor() -> None:
                 or active_ffmpeg_process.poll() is not None
             ):
                 monitor_thread_running = False
-                print("Since the process has stopped, monitoring will be terminated.")
+                logger.warning(
+                    "Since the process has stopped, monitoring will be terminated"
+                )
                 break
 
             elapsed = time.time() - last_access_time
@@ -187,19 +211,21 @@ def monitor() -> None:
     if not monitor_thread_running and active_ffmpeg_process:
         stop()
 
+    logger.info("Stoped timeout monitor thread")
 
-def transfer(stream_fd, ffmpeg_proc) -> None:
-    print("Started transferring data from Streamlink to FFmpeg.")
+
+def transfer(stream_fd: StreamIO, ffmpeg_proc: subprocess.Popen[bytes]) -> None:
+    logger.info("Starting transferring data from Streamlink to FFmpeg")
 
     try:
         while bridge_thread_running:
             data = stream_fd.read(BUFF_SIZE)
             if not data:
-                print("The end of the stream has been reached.")
+                logger.info("End of the stream has been reached")
                 break
 
             if ffmpeg_proc is None or ffmpeg_proc.stdin is None:
-                print("ffmpeg process or stdin is unavailable.")
+                logger.warning("FFmpeg process or stdin is unavailable")
                 break
 
             elif ffmpeg_proc.poll() is None:
@@ -210,7 +236,7 @@ def transfer(stream_fd, ffmpeg_proc) -> None:
                 break
 
     except (BrokenPipeError, OSError, ValueError, TypeError) as e:
-        print(f"Error transfering stream: {e}")
+        logger.error(f"Streaming data transfer error: {e}")
 
     finally:
         if ffmpeg_proc is not None and ffmpeg_proc.stdin is not None:
@@ -220,7 +246,7 @@ def transfer(stream_fd, ffmpeg_proc) -> None:
             except BrokenPipeError, OSError, ValueError:
                 pass
 
-        print("Ended transferring data from Streamlin to FFmpeg.")
+        logger.info("Stopped transferring data")
 
 
 def update_access_time() -> None:
@@ -283,6 +309,8 @@ def main() -> None:
 
     if os.path.exists(HLS_DIR):
         shutil.rmtree(HLS_DIR)
+
+    logger.info(f"Starting radiko restream server {APP_VERSION}")
 
     app.run(
         host=HOST,
